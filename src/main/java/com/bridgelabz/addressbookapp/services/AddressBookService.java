bae61fa377773762549bd0ff926e350aaee64a5f
suraj patel <sp9589890205@gmail.com>
2025-02-24 13:21:02 +0530

    [Suraj] add. AddressBook Service layer added

---
+++ src/main/java/com/bridgelabz/addressbookapp/services/AddressBookService.java
@@ -0,0 +1,56 @@
+package com.bridgelabz.addressbookapp.services;
+
+import com.bridgelabz.addressbookapp.model.AddressBook;
+import com.bridgelabz.addressbookapp.repository.AddressBookRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+@Service
+public class AddressBookService {
+    @Autowired
+    AddressBookRepo addressBookRepository;
+
+    public List<AddressBook> getAllAddresses(){
+        return addressBookRepository.findAll();
+    }
+
+    //get address by id
+    public Optional<AddressBook> getAddressById(Long id){
+        return addressBookRepository.findById(id);
+    }
+
+    //save the address
+    public AddressBook addAddress(AddressBook addressBook){
+        return addressBookRepository.save(addressBook);
+    }
+
+    //Update address
+    public AddressBook updateAddress(Long id, AddressBook updateAddressBook){
+        Optional<AddressBook> optionalAddressBook = getAddressById(id);
+        if(optionalAddressBook.isPresent()){
+            AddressBook addressBook = optionalAddressBook.get();
+            addressBook.setName(updateAddressBook.getName());
+            addressBook.setAddress(updateAddressBook.getAddress());
+            addressBook.setPhoneNo(updateAddressBook.getPhoneNo());
+            addressBook.setEmail(updateAddressBook.getEmail());
+            return addressBookRepository.save(addressBook);
+        }
+        else{
+            return  null;
+        }
+    }
+
+    //Delete Address
+    public String deleteAddressById(Long id){
+        Optional<AddressBook> optionalAddressBook = getAddressById(id);
+        if(optionalAddressBook.isPresent()) {
+            addressBookRepository.deleteById(id);
+            return "Address Delete Successful";
+        }
+        else{
+            return null;
+        }
+    }
+}
