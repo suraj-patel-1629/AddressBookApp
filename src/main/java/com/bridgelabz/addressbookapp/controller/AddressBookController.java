44b13908986f0e4d2f05eff06ecf215706ee9142
suraj patel <sp9589890205@gmail.com>
2025-02-24 13:18:33 +0530

    [Suraj] add. AddressBook Controller Added

---
+++ src/main/java/com/bridgelabz/addressbookapp/controller/AddressBookController.java
@@ -0,0 +1,51 @@
+package com.bridgelabz.addressbookapp.controller;
+
+import com.bridgelabz.addressbookapp.model.AddressBook;
+import com.bridgelabz.addressbookapp.services.AddressBookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/address")
+public class AddressBookController {
+    @Autowired
+    AddressBookService addressBookService;
+
+    //To get all the addresses stored in the database
+    @GetMapping("/getall")
+    public List<AddressBook> getAllAddress(){
+
+        return addressBookService.getAllAddresses();
+    }
+
+    //To get the address by id
+    @GetMapping("/get/{id}")
+    public Optional<AddressBook> getAddressById(@PathVariable Long id){
+
+        return addressBookService.getAddressById(id);
+    }
+
+    //To store the new address in database
+    @PostMapping("/create")
+    public AddressBook saveAddress(@RequestBody AddressBook addressBook){
+
+        return addressBookService.addAddress(addressBook);
+    }
+
+    //To update the existing address in the database
+    @PutMapping("/update/{id}")
+    public AddressBook updateAddress(@PathVariable Long id,@RequestBody AddressBook addressBook){
+
+        return addressBookService.updateAddress(id,addressBook);
+    }
+
+    //To delete the address from db
+    @DeleteMapping("/delete/{id}")
+    public String deleteAddress(@PathVariable Long id){
+
+        return addressBookService.deleteAddressById(id);
+    }
+}
